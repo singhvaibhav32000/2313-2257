e2f161577685333ae7fd829f3faf55dff18ef271
singhvaibhav32000 <43774014+singhvaibhav32000@users.noreply.github.com>
2018-10-02 14:43:32 +0530

    Add files via upload

---
+++ git/oppsclass2.py
@@ -0,0 +1,11 @@
+#class Student:
+   # def __init__(selfless):
+    #    selfless.name = 'vaibhav'
+   #     selfless.age = 12
+  #  def display(selfless):
+ #       print(selfless.name)
+#        print(selfless.age)
+#s1 = Student()
+#s1.display(
+for i in range(10):
+    print("{0} {0} {0} {0}".format(i,hex))
